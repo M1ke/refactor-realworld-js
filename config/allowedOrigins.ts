525f70286f0139334afcc64272836d8a29734f6f
m1ke <mike@m1ke.me>
2026-06-09 10:02:01 +0000

    Convert config/allowedOrigins.js to TypeScript

---
+++ config/allowedOrigins.ts
@@ -0,0 +1,8 @@
+const allowedOrigins: string[] = [
+    'http://localhost:3000',
+    'http://localhost:4200',
+    'https://winterrrrrff.github.io',
+    'https://angular-realworld-example-app-neon.vercel.app'
+];
+
+export default allowedOrigins;
